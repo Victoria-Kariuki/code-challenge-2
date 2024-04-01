99f955d834aa547a5a469ae251ebf94238eeb238
victoria <kariukivictoria428@gmail.com>
2024-04-01 15:45:38 +0300

    challenge3

---
+++ index.js
@@ -0,0 +1,8 @@
+//finding of prime numbers in an array
+var numbers = (SVGAnimatedNumberList)
+
+const primeNumbers = (primeNumbers);
+const length = (no.ofPrimeNumbers)
+
+console.log(SVGAnimatedNumberList(1 , 10))
+console.log(primeNumbers(2,3,5,7))
